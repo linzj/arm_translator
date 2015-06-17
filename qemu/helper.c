47da593e11698bbb6ab2c9c9e134d15114e3cfa1
linzj <linzj@ucweb.com>
2015-06-17 18:39:55 +0800

    Use qemu helper.

--- qemu/helper.c
+++ qemu/helper.c
@@ -1,6 +1,8 @@
 #include "helper-proto.h"
 #include "log.h"
 
+#define SIGNBIT (uint32_t)0x80000000
+#define SIGNBIT64 ((uint64_t)1 << 63)
 static void cpu_loop_exit(CPU* cpu)
 {
     longjmp(cpu->exit_buf, 1);
@@ -40,4 +42,82 @@ void HELPER(access_check_cp_reg)(CPUARMState* env, void* rip, uint32_t syndrome)
         EMUNREACHABLE();
     }
     raise_exception(env, EXCP_UDEF);
+}
+
+uint32_t HELPER(neon_tbl)(CPUARMState* env, uint32_t ireg, uint32_t def,
+    uint32_t rn, uint32_t maxindex)
+{
+    uint32_t val;
+    uint32_t tmp;
+    int index;
+    int shift;
+    uint64_t* table;
+    table = (uint64_t*)&env->vfp.regs[rn];
+    val = 0;
+    for (shift = 0; shift < 32; shift += 8) {
+        index = (ireg >> shift) & 0xff;
+        if (index < maxindex) {
+            tmp = (table[index >> 3] >> ((index & 7) << 3)) & 0xff;
+            val |= tmp << shift;
+        }
+        else {
+            val |= def & (0xff << shift);
+        }
+    }
+    return val;
+}
+
+uint32_t HELPER(add_saturate)(CPUARMState* env, uint32_t a, uint32_t b)
+{
+    uint32_t res = a + b;
+    if (((res ^ a) & SIGNBIT) && !((a ^ b) & SIGNBIT)) {
+        env->QF = 1;
+        res = ~(((int32_t)a >> 31) ^ SIGNBIT);
+    }
+    return res;
+}
+
+uint32_t HELPER(add_setq)(CPUARMState* env, uint32_t a, uint32_t b)
+{
+    uint32_t res = a + b;
+    if (((res ^ a) & SIGNBIT) && !((a ^ b) & SIGNBIT))
+        env->QF = 1;
+    return res;
+}
+
+uint32_t HELPER(sub_saturate)(CPUARMState* env, uint32_t a, uint32_t b)
+{
+    uint32_t res = a - b;
+    if (((res ^ a) & SIGNBIT) && ((a ^ b) & SIGNBIT)) {
+        env->QF = 1;
+        res = ~(((int32_t)a >> 31) ^ SIGNBIT);
+    }
+    return res;
+}
+
+uint32_t HELPER(double_saturate)(CPUARMState* env, int32_t val)
+{
+    uint32_t res;
+    if (val >= 0x40000000) {
+        res = ~SIGNBIT;
+        env->QF = 1;
+    }
+    else if (val <= (int32_t)0xc0000000) {
+        res = SIGNBIT;
+        env->QF = 1;
+    }
+    else {
+        res = val << 1;
+    }
+    return res;
+}
+
+uint32_t HELPER(add_usaturate)(CPUARMState* env, uint32_t a, uint32_t b)
+{
+    uint32_t res = a + b;
+    if (res < a) {
+        env->QF = 1;
+        res = ~0;
+    }
+    return res;
 }
