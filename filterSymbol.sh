08b9aa8691b60ec19bc0abee61bc3eabf666abd1
linzj <linzj@ucweb.com>
2015-06-23 11:28:36 +0800

    Add a test script.

---
+++ filterSymbol.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+valgrind --tool=callgrind  --callgrind-out-file=call.out ./out/Debug/testQEMU tests/*.txt
+if ! [ -f call.out ]
+    then
+        echo "not callgrind output" >2
+fi
+for func in `cat symbols_to_be_tested`
+    do
+        if  grep $func call.out >/dev/null 2>/dev/null
+            then
+                sed -i "/$func/d" symbols_to_be_tested
+                echo "Removed symbol $func"
+        fi
+done
+rm call.out
